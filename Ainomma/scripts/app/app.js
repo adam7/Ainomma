67013b0dc18e7613571a4033376cc77f694366c7
Adam Cooper <adam@sequence7.net>
2014-12-04 22:30:40 +0000

    Sort of getting the item page working a bit :woot:

--- Ainomma/scripts/app/app.js
+++ Ainomma/scripts/app/app.js
@@ -6,20 +6,21 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider.state('home', {
         url: '/',
-        views: {
-            home: {
-                templateUrl: 'home.html',
-                controller: 'FrontPageController'
-            }
-        }
+        templateUrl: 'home.html',
+        controller: 'FrontPageController'
     })
 
-    $stateProvider.state('help', {
-        url: '/help',
-        views: {
-            help: {
-                templateUrl: 'help.html'
-            }
+    $stateProvider.state('about', {
+        url: '/about',
+        templateUrl: 'about.html'
+    })
+
+    $stateProvider.state('item', {
+        url: '/item/:itemId',
+        templateUrl: 'item.html',
+        controller: 'ItemController',
+        resolve: {
+            // TODO: Need to resolve the todo with a service from here ... 
         }
     })
 })
@@ -33,24 +34,45 @@ app.controller('FrontPageController', ['$firebase', '$scope', '$ionicLoading', f
 
     // Start with an empty array of items, this is what angular will bind to 
     $scope.items = [];
-    
+
     // Get a reference to the HN api
     var ref = new Firebase("https://hacker-news.firebaseio.com/v0/topstories");
 
     ref.on("child_added", onChildAdded);
-    
+
     function onChildAdded(snapshot) {
         var ref = new Firebase("https://hacker-news.firebaseio.com/v0/item/" + snapshot.val());
 
-        ref.once("value", addOnce);
+        ref.once("value", onGetItem);
     }
 
-    function addOnce(data) {
+    function onGetItem(data) {
         $scope.items.push(data.val());
-        console.log(data.val());
+        //console.log(data.val());
     }
 }]);
 
-app.controller('UsersController', ['$firebase', '$scope', function ($firebase, $scope) {
-    var ref = new Firebase("https://hacker-news.firebaseio.com/v0/user/");
+app.controller('ItemController', ['$firebase', '$scope', '$stateParams', function ($firebase, $scope, $stateParams) {
+    var itemId = $stateParams.itemId;
+
+    globalScope = $scope;
+
+    $scope.comments = [];
+    $scope.title = "title"
+
+    var ref = new Firebase("https://hacker-news.firebaseio.com/v0/item/" + itemId);
+
+    ref.once("value", onGetItem);
+    
+    function onGetItem(data) {
+        console.log(data.val());
+        globalData = data;
+
+        $scope.title = data.val().title;
+        $scope.description = data.val().by;
+        $scope.comments = data.val().kids;
+
+        console.log($scope.title);
+    }
 }]);
+var globalScope;
